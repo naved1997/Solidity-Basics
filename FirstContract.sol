a537612d22b327d50b1f795eb2d140497f0c29f6
Naved Khan <94339342+naved1997@users.noreply.github.com>
2022-05-31 20:30:34 +0530

    Create FirstContract.sol

---
+++ FirstContract.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+
+//License is for copyright purposes as smart contract are available to everyone to read, 
+// if not written it will give warnings but not any error
+
+pragma solidity ^0.8.0;
+
+//this is for defining version of solidity compiler should use
+
+
+contract FirstContract{
+    //this is how contract is written.
+}
